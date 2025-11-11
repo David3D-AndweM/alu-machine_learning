c4db088ec4c6ee20df1172c5241c344b3137a749
David Mwape <d.mwape@alustudent.com>
2025-11-11 18:50:48 +0200

    Task 11: The Western Exchange - NumPy transpose

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module for numpy array transpose
+"""
+
+
+def np_transpose(matrix):
+    """
+    Transposes a numpy.ndarray
+    
+    Args:
+        matrix: A numpy.ndarray
+    
+    Returns:
+        A new numpy.ndarray that is the transpose
+    """
+    return matrix.T
