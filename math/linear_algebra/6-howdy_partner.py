ba93a1fc96c07b199a6a3e3e565c9258759e8ad8
David Mwape <d.mwape@alustudent.com>
2026-01-21 08:24:52 +0200

    task 6

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,18 +1,4 @@
 #!/usr/bin/env python3
-"""
-Module for array concatenation
-"""
-
 
 def cat_arrays(arr1, arr2):
-    """
-    Concatenates two arrays
-    
-    Args:
-        arr1: First array (list of ints/floats)
-        arr2: Second array (list of ints/floats)
-    
-    Returns:
-        A new list containing elements from both arrays
-    """
     return arr1 + arr2
