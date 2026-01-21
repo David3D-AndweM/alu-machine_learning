a0591e72504b09b8555fc87b6ca226cd278dc289
David Mwape <d.mwape@alustudent.com>
2026-01-21 08:27:13 +0200

    task 12

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,23 +1,4 @@
 #!/usr/bin/env python3
-"""
-Module for numpy element-wise operations
-"""
-
 
 def np_elementwise(mat1, mat2):
-    """
-    Performs element-wise addition, subtraction, multiplication, and division
-    
-    Args:
-        mat1: First numpy.ndarray
-        mat2: Second numpy.ndarray or scalar
-    
-    Returns:
-        A tuple containing (sum, difference, product, quotient)
-    """
-    add = mat1 + mat2
-    sub = mat1 - mat2
-    mul = mat1 * mat2
-    div = mat1 / mat2
-    
-    return (add, sub, mul, div)
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
