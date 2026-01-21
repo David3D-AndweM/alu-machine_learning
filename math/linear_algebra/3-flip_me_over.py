e2ade9aa2a114d4ca7c3568953c0f2b47cfe0e48
David Mwape <d.mwape@alustudent.com>
2026-01-21 09:34:29 +0200

    fix task 3 documentation and pycodestyle

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""Module for matrix transpose"""
+
 
 def matrix_transpose(matrix):
-    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    """Transpose a 2D matrix"""
+    rows = len(matrix)
+    cols = len(matrix[0])
+    return [[matrix[j][i] for j in range(rows)] for i in range(cols)]
