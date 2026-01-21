628441eaa212038ee5aecb25291fb1861ceb0a1b
David Mwape <d.mwape@alustudent.com>
2026-01-21 08:23:42 +0200

    task 3

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,31 +1,4 @@
 #!/usr/bin/env python3
-"""
-Module for matrix transpose
-"""
-
 
 def matrix_transpose(matrix):
-    """
-    Returns the transpose of a 2D matrix
-    
-    Args:
-        matrix: A 2D matrix (list of lists)
-    
-    Returns:
-        A new matrix that is the transpose of the input matrix
-    """
-    if not matrix or not matrix[0]:
-        return []
-    
-    rows = len(matrix)
-    cols = len(matrix[0])
-    
-    # Create new matrix with swapped dimensions
-    transpose = []
-    for j in range(cols):
-        new_row = []
-        for i in range(rows):
-            new_row.append(matrix[i][j])
-        transpose.append(new_row)
-    
-    return transpose
+    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
