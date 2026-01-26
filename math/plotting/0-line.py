da251342d9179a8014d7f128c2d3f112ed91d758
David Mwape <d.mwape@alustudent.com>
2026-01-26 07:10:35 +0200

    Complete task 0: line graph plotting

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python3
-"""
-Module for plotting a line graph.
-
-This module contains code to plot y = x^3 as a solid red line
-with x-axis ranging from 0 to 10.
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 y = np.arange(0, 11) ** 3
 
-# Plot y as a solid red line
 plt.plot(y, 'r-')
-
-# Set x-axis range from 0 to 10
 plt.xlim(0, 10)
-
-# Display the plot
 plt.show()
